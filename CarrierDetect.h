091e423d9818330bfdea57e168bf1818f4fefbb9
Rob Riggs <rob@pangalactic.org>
2020-11-30 18:52:12 -0600

    Simplify CarrierDetect.

--- CarrierDetect.h
+++ CarrierDetect.h
@@ -2,6 +2,8 @@
 
 #pragma once
 
+#include "IirFilter.h"
+
 #include <array>
 #include <algorithm>
 #include <numeric>
@@ -10,47 +12,28 @@
 namespace mobilinkd
 {
 
-template <typename FloatType, size_t N = 10>
+template <typename FloatType>
 struct CarrierDetect
 {
     using result_t = std::tuple<bool, FloatType>;
 
+    BaseIirFilter<FloatType, 3> filter_;
     FloatType lock_;
     FloatType unlock_;
-    std::array<FloatType, N> samples_;
-    std::array<FloatType, N> variances_;
-    FloatType sum_ = 0.0;
-    FloatType var_ = 0.0;
-    size_t index_ = 0;
     bool locked_ = false;
 
-    CarrierDetect(FloatType lock_level, FloatType unlock_level)
-    : lock_(lock_level), unlock_(unlock_level)
+    CarrierDetect(std::array<FloatType, 3> const& b, std::array<FloatType, 3> const& a, FloatType lock_level, FloatType unlock_level)
+    : filter_(b, a), lock_(lock_level), unlock_(unlock_level)
     {
-        samples_.fill(0.0);
     }
     
-    result_t operator()(FloatType evm)
+    result_t operator()(FloatType value)
     {
-        sum_ = sum_ - samples_[index_] + evm;
-        auto var = evm - (sum_ / N);
-        var_ = var_ - variances_[index_] + (var * var);
-        variances_[index_] = (var * var);
-        samples_[index_++] = evm;
-        if (index_ == N) index_ = 0;
-        
-        auto variance = var_ / N;
-        auto stdev = sqrt(variance);
-        if (!locked_)
-        {
-            if (stdev < lock_) locked_ = true;
-        }
-        else
-        {
-            if (stdev > unlock_) locked_ = false;
-        }
+        auto filtered = filter_(std::abs(value));
+        if (locked_ & filtered > unlock_) locked_ = false;
+        else if (!locked_ & filtered < lock_) locked_ = true;
 
-        return std::make_tuple(locked_, stdev);
+        return std::make_tuple(locked_, filtered);
     }
 };
 
