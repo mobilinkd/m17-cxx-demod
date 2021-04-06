81c2b0466e11a248a9addd0223868f797cd572de
Rob Riggs <rob@pangalactic.org>
2021-04-05 20:04:02 -0500

    Add unit tests for callsign encoding and decoding and change the encoding scheme to use big-endian encoding.

--- LinkSetupFrame.h
+++ LinkSetupFrame.h
@@ -6,6 +6,7 @@
 #include <cstdint>
 #include <string_view> // Don't have std::span in C++17.
 #include <stdexcept>
+#include <algorithm>
 
 namespace mobilinkd
 {
@@ -27,6 +28,20 @@ struct LinkSetupFrame
     DataType data_type_ = DataType::VOICE;
     EncType encryption_type_ = EncType::NONE;
 
+    /**
+     * The callsign is encoded in base-40 starting with the right-most
+     * character.  The final value is written out in "big-endian" form, with
+     * the most-significant value first.  This leads to 0-padding of shorter
+     * callsigns.
+     *
+     * @param[in] callsign is the callsign to encode.
+     * @param[in] strict is a flag (disabled by default) which indicates whether
+     *  invalid characters are allowed and assugned a value of 0 or not allowed,
+     *  resulting in an exception.
+     * @return the encoded callsign as an array of 6 bytes.
+     * @throw invalid_argument when strict is true and an invalid callsign (one
+     *  containing an unmappable character) is passed.
+     */
     static encoded_call_t encode_callsign(call_t callsign, bool strict = false)
     {
         // Encode the characters to base-40 digits.
@@ -65,18 +80,22 @@ struct LinkSetupFrame
         const auto p = reinterpret_cast<uint8_t*>(&encoded);
 
         encoded_call_t result;
-        std::copy(p, p + 6, result.begin());
+        std::copy(p, p + 6, result.rbegin());
         
         return result;
     }
 
+    /**
+     * Decode a base-40 encoded callsign to its text representation.  This decodes
+     * a 6-byte big-endian value into a string of up to 9 characters.
+     */
     static call_t decode_callsign(encoded_call_t callsign, bool strict = false)
     {
         static const char callsign_map[] = "xABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-/.";
 
         uint64_t encoded = 0;       // This only works on little endian architectures.
         auto p = reinterpret_cast<uint8_t*>(&encoded);
-        std::copy(callsign.begin(), callsign.end(), p);
+        std::copy(callsign.rbegin(), callsign.rend(), p);
 
         // decode each base-40 digit and map them to the appriate character.
         call_t result;
